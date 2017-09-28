f2ad006ba793718e3905ed12886747d365a581fa
Prasasto Adi <prasastoadi@gmail.com>
2017-09-28 22:13:54 +0700

    Update TEXT

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,8 @@
-TEXT = """Kami belajar tanpa lelah."""
+TEXT = """Kami belajar tanpa lelah.
+Kami& tidur tak teratur.
+Kami kuliah dengan giat.
+Kami korbankan masa#muda.
+Itu karena kami ingin kelak istri dan anak kami bahagia."""
 
 def tokenize(text):
     text = text.lower()
